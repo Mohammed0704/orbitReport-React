78885b23f05b5f7619ac045a9149a11f62b088c8
Mohammed0704 <116527392+Mohammed0704@users.noreply.github.com>
2023-08-11 16:44:30 -0400

    Final Commits

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+const Banner = () => {
+    return (
+    <main>
+        <header>
+            <h1>Orbit Report</h1>
+        </header>
+        <p>
+           Click on the buttons to see the satellites in that orbit type
+        </p>
+    </main>
+    );
+};
+
+export default Banner;
